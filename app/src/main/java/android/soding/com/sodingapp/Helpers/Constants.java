93723e64525555f11f0572e52dfb44e407a150e9
Mohamed Abd-Allah <midoalageb@gmail.com>
2017-09-30 15:59:19 +0200

    Bug: Remove Constants by mistake

--- app/src/main/java/android/soding/com/sodingapp/Helpers/Constants.java
+++ app/src/main/java/android/soding/com/sodingapp/Helpers/Constants.java
@@ -8,4 +8,7 @@ public class Constants {
     public static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss"; //Date format for DB and display
     public static final String DB_NAME = "Tasks_DB.db"; //Live DB name
     public static final String DB_NAME_TEST = "Tasks_Test_DB.db"; //Test DB name
+    public static final String ACTION_UPDATE = "ACTION_UPDATE"; //Test DB name
+    public static final String ACTION_DELETE = "ACTION_DELETE"; //Test DB name
+
 }
